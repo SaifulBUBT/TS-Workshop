2a575178b704b535f36c74e9b83afecfef8da507
Md. Saiful Islam <42247667+SaifulBUBT@users.noreply.github.com>
2026-07-27 21:02:38 +0600

    feat: typeScript worksohp initialize

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const message = "Hello World!";
+console.log(message);
+const isAdmin = true;
+console.log(isAdmin);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const message: string = "Hello World!";
+
+console.log(message);
+
+const isAdmin: boolean = true;
+console.log(isAdmin);
